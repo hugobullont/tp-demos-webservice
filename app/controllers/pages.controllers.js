fa080edb13ffd05906ca75281412484275f3ae8a
hugobullont <hugobullont@gmail.com>
2019-03-29 18:50:31 -0500

    fix bugs

--- app/controllers/pages.controllers.js
+++ app/controllers/pages.controllers.js
@@ -2,7 +2,7 @@ let page = '';
 
 exports.get = (req, res) => {
     console.log('Sending HTML PAGE');
-    var json = JSON.stringify({html = page});
+    var json = JSON.stringify({html: page});
     res.send(json);
 }
 
